0c83c32efff8d3a8c99a0f9c7633cd1ad85a1625
manan-g <59761662+manan-g@users.noreply.github.com>
2021-09-16 21:50:35 +0530

    create folder structure,  main components empty files,  setup routes (#1)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Pages/home';
+import Reddit from './Pages/reddit';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div>
-      App Placeholder
+      <Header />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/app">
+            <Reddit />
+          </Route>
+        </Switch>
+      </Router>
+      <Footer />
     </div>
   );
 }
