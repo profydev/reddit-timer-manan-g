0e6a1ad4f6492b2d87b19e45c7ef05710a0b093a
manan-g <59761662+manan-g@users.noreply.github.com>
2021-09-22 23:50:29 +0530

    fix: linting errors in header tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router';
+import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
